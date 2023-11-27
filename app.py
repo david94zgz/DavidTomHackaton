ed7390fbb7e2525058eee7bc86f8c16c41ebec83
davidlahozeslamdoost <davidlahozeslamdoost@nl.abnamro.com>
2023-11-27 14:48:24 +0100

    idontknow

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
+
+
+cars = [{
+    "licence_plate": "XBD9543D",
+    "brand": "Toyota",
+    "color": "Black",
+    "price": 10
+}]
 
 
 @app.route("/")
