cd7659e00537f78e6caf31d69e38ae18d667c4f3
davidlahozeslamdoost <davidlahozeslamdoost@nl.abnamro.com>
2023-11-27 15:35:33 +0100

    d

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
 from flask import Flask, render_template
 import pandas
+import json
 
 app = Flask(__name__)
 
-
-cars = [{
-    "licence_plate": "XBD9543D",
-    "brand": "Toyota",
-    "color": "Black",
-    "price": 10
-}]
 
 df = pandas.read_csv("data/vgsales.csv")
 
 @app.route("/")
 def home():
-    return df.to_json()
+    result = {
+    "num_entries": df.size,
+        "data": 
+            [json.loads(x) for x in df['json'].to_dict().values()]
+    }
+    return result
+
+@app.route("genre/<string:genre>")
+def get_genre(genre):
+    df_selection = df.loc[df["Genre"] == genre]
+    result = {
+        "num_entries": df_selection.size,
+        "data": 
+            [json.loads(x) for x in df_selection['json'].to_dict().values()]
+    }
+    return result
