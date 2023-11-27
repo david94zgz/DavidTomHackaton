1dda79f8a92d8098ca81f91b442eab209c40df22
davidlahozeslamdoost <davidlahozeslamdoost@nl.abnamro.com>
2023-11-27 15:59:23 +0100

    something

--- app.py
+++ app.py
@@ -1,26 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask
 import pandas
 import json
+from datetime import datetime
 
 app = Flask(__name__)
 
 
 df = pandas.read_csv("data/vgsales.csv")
+df['json'] = df.apply(lambda x:x.to_json(), axis=1)
+
 
 @app.route("/")
 def home():
     result = {
-    "num_entries": df.size,
+        "a_number_entries": str(df.size),
+        "a_date_request": str(datetime.today()),
         "data": 
             [json.loads(x) for x in df['json'].to_dict().values()]
     }
     return result
 
-@app.route("genre/<string:genre>")
-def get_genre(genre):
-    df_selection = df.loc[df["Genre"] == genre]
+@app.route("/<string:category>/<string:filter>")
+def get_category(category, filter):
+    df_selection = df.loc[df[category.capitalize()] == filter]
     result = {
-        "num_entries": df_selection.size,
+        "a_number_entries": str(df_selection.size),
+        "a_date_request": str(datetime.today()),
         "data": 
             [json.loads(x) for x in df_selection['json'].to_dict().values()]
     }
